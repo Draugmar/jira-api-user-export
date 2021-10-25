eae4ef2a483170a2f8108b3728c0ea2731942621
Valkrieg <10279125+Valkrieg@users.noreply.github.com>
2021-10-25 09:00:36 +0200

    Py file uploaded

---
+++ jira-api-user-export.py
@@ -0,0 +1,48 @@
+import requests, json, csv
+from requests.auth import HTTPBasicAuth
+
+JIRA_ENDPOINT_BASE = "https://yourdomain.atlassian.net"
+
+headers = {"Accept": "application/json"}
+auth = HTTPBasicAuth("account@yourdomain.com", "yourApiToken")
+
+CHUNK_SIZE = 1000
+query = {
+    "startAt": -CHUNK_SIZE,
+    "maxResults": CHUNK_SIZE,
+}
+
+users = {}
+
+while True:
+    query["startAt"] += CHUNK_SIZE
+    print("req", query["startAt"])
+    req = requests.request(
+        "GET",
+        f"{JIRA_ENDPOINT_BASE}/rest/api/3/users/search",
+        headers=headers,
+        auth=auth,
+        params=query,
+    )
+    data = json.loads(req.text)
+
+    if len(data) == 0:
+        break  # Last page reached
+
+    for chunk in data:
+        aid = chunk["accountId"]
+        if aid in users.keys():
+            break  # Last page reached
+        else:
+            users[aid] = chunk
+
+f = open('jiraBulk.csv', 'w', encoding="UTF8")
+writer = csv.writer(f)
+
+for user in users.values():
+    name = user.get("displayName", "Username not found")
+    id = user.get("accountId", "Account Id not found")
+    email = user.get("emailAddress", "Mail address not found")
+    writer.writerow([name, id, email])
+
+f.close()
